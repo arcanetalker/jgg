672237d475adfc1fd6cd8ddfac39d28b760548c2
arcanetalker <76554828+arcanetalker@users.noreply.github.com>
2021-05-12 14:41:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+ 
+  
+  bg= loadImage("cave.png")
+  bg2=loadImage("treasure.jpg")
+  bg2.scale = 2;
+}
+
+function setup() {
+  createCanvas(1000,500);
+  security = new Security();
+  system = new System();
+  
+  
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+  
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    stroke("black")
+    strokeWeight("10")
+    fill("white")
+    textSize(60);
+    text("Treasure unlocked !!!",250, 270);
+  }
+   
+  
+
+  drawSprites()
+}
